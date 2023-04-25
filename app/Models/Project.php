d423159c67a20c96b4aaa8ee1c2cbac75077ca83
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-04-25 20:43:51 +0200

    add pagination % getAbstract

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'image', 'text', 'link'];
+
+    public function getAbstract($max = 50) {
+        return substr($this->text, 0, $max). "...";
+    }
 }
