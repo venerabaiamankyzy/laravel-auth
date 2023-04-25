a77572334cbce1a3e006b866efdbc6f14a448cf2
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-04-25 20:41:04 +0200

    add setPasswordAttribute

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,7 +42,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    protected function setPasswordAttribute($value) {
+     protected function setPasswordAttribute($value) {
         $this->attributes['password'] = bcrypt($value);
     }
 }
