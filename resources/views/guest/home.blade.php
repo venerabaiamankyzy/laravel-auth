d7dd175cb49b9e97c97bd6beeb6a256908a84d8d
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-04-25 09:10:38 +0200

    add file guest with home blade

---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.guest')
+@section('content')
+
+@section('content')
+<div class="container">
+    <h2 class="fs-4 text-secondary my-4">
+        {{ __('Dashboard') }}
+    </h2>
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">{{ __('User Dashboard') }}</div>
+
+                <div class="card-body">
+                  Guest
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@endsection
